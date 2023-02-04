bfbf5871895f7ade7e5a30d6ceedf0c5ba18279e
Jose A Alvarado <jaa2220@cumc.columbia.edu>
2023-02-04 17:42:50 -0500

    Remove files related to pyneoingest pakcage

--- pyneoinstance/database/context.py
+++ pyneoinstance/database/context.py
@@ -3,4 +3,4 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath(
     os.path.join(os.path.dirname(__file__), '..')))
-from pyneoingest.util.functions import get_logger
+from pyneoinstance.util.functions import get_logger
