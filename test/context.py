cea0173884d792e2b4473eeb9d2ae371644c6746
Jose A Alvarado <jaa2220@cumc.columbia.edu>
2023-02-04 17:25:51 -0500

    Change the name of the main pakage in the context file of the tests

--- test/context.py
+++ test/context.py
@@ -3,6 +3,6 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath(
     os.path.join(os.path.dirname(__file__), '..')))
-from pyneoingest import fileload
-from pyneoingest import util
-from pyneoingest import database
+from pyneoinstance import fileload
+from pyneoinstance import util
+from pyneoinstance import database
